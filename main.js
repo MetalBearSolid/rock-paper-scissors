ee3241b580ef1e0794312e2cf86c72d1b317d280
Simon Seto <simonseto611@hotmail.com>
2019-06-30 10:37:58 -0700

    added double quotes from prettier extension

--- main.js
+++ main.js
@@ -1,85 +1,87 @@
 //This will grab all the .choice class and puts it into an Array
-const choices = document.querySelectorAll('.choice');
-const score = document.getElementById('score');
-const result = document.getElementById('result');
-const restart = document.getElementById('restart');
-const modal = document.querySelector('.modal');
+const choices = document.querySelectorAll(".choice");
+const score = document.getElementById("score");
+const result = document.getElementById("result");
+const restart = document.getElementById("restart");
+const modal = document.querySelector(".modal");
 const scoreboard = {
   player: 0,
   computer: 0
-}
+};
 
 // Play game
 function play(e) {
-  restart.style.display = 'inline-block'
+  restart.style.display = "inline-block";
   const playerChoice = e.target.id;
   const computerChoice = getComputerChoice();
   const winner = getWinner(playerChoice, computerChoice);
   showWinner(winner, computerChoice);
-  
 }
 
 // Get computers chioce
 function getComputerChoice() {
   const rand = Math.random();
-  if(rand < 0.34) {
-    return 'rock';
+  if (rand < 0.34) {
+    return "rock";
   } else if (rand <= 0.67) {
-    return 'paper';
+    return "paper";
   } else {
-    return 'scissors';
+    return "scissors";
   }
 }
 
 // Get game winner
 function getWinner(p, c) {
-  if(p === c) {
-    return 'draw';
-  } else if(p === 'rock') {
-    if(c === 'paper') {
-      return 'computer';
+  if (p === c) {
+    return "draw";
+  } else if (p === "rock") {
+    if (c === "paper") {
+      return "computer";
     } else {
-      return 'player';
+      return "player";
     }
-  } else if(p === 'paper') {
-    if(c === 'scissors') {
-      return 'computer';
+  } else if (p === "paper") {
+    if (c === "scissors") {
+      return "computer";
     } else {
-      return 'player';
+      return "player";
     }
-  } else if(p === 'scissors') {
-    if(c === 'rock') {
-      return 'computer';
+  } else if (p === "scissors") {
+    if (c === "rock") {
+      return "computer";
     } else {
-      return 'player';
+      return "player";
     }
   }
 }
 
 function showWinner(winner, computerChoice) {
-  if(winner === 'player') {
+  if (winner === "player") {
     // Increment playr score
     scoreboard.player++;
     // show modal result
     result.innerHTML = `
     <h1 class="text-win">You win!</h1>
     <i class="fas fa-hand-${computerChoice} fa-10x"></i>
-    <p>Computer Chose <strong>${computerChoice.charAt(0).toUpperCase() + computerChoice.slice(1)}</strong></p>
+    <p>Computer Chose <strong>${computerChoice.charAt(0).toUpperCase() +
+      computerChoice.slice(1)}</strong></p>
     `;
-  } else if(winner === 'computer') {
-     // Increment playr score
-     scoreboard.computer++;
-     // show modal result
-     result.innerHTML = `
+  } else if (winner === "computer") {
+    // Increment playr score
+    scoreboard.computer++;
+    // show modal result
+    result.innerHTML = `
      <h1 class="text-win">You lose!</h1>
      <i class="fas fa-hand-${computerChoice} fa-10x"></i>
-     <p>Computer Chose <strong>${computerChoice.charAt(0).toUpperCase() + computerChoice.slice(1)}</strong></p>
+     <p>Computer Chose <strong>${computerChoice.charAt(0).toUpperCase() +
+       computerChoice.slice(1)}</strong></p>
      `;
   } else {
     result.innerHTML = `
      <h1>It's a Draw!</h1>
      <i class="fas fa-hand-${computerChoice} fa-10x"></i>
-     <p>Computer Chose <strong>${computerChoice.charAt(0).toUpperCase() + computerChoice.slice(1)}</strong></p>
+     <p>Computer Chose <strong>${computerChoice.charAt(0).toUpperCase() +
+       computerChoice.slice(1)}</strong></p>
      `;
   }
 
@@ -89,7 +91,7 @@ function showWinner(winner, computerChoice) {
   <p>Computer: ${scoreboard.computer}</p>
   `;
 
-  modal.style.display = 'block';
+  modal.style.display = "block";
 }
 
 // Restart game
@@ -104,12 +106,12 @@ function restartGame() {
 
 // Clear Modal
 function clearModal(e) {
-  if(e.target == modal) {
-    modal.style.display = 'none';
+  if (e.target == modal) {
+    modal.style.display = "none";
   }
 }
 
 // Event Listeners
-choices.forEach(choice => choice.addEventListener('click', play));
-window.addEventListener('click', clearModal);
-restart.addEventListener('click', restartGame);
+choices.forEach(choice => choice.addEventListener("click", play));
+window.addEventListener("click", clearModal);
+restart.addEventListener("click", restartGame);
